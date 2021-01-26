cbdd91c5c4f86336b66a9191f4dcf63e41089650
Miguel Angel -Cheroki- <miguelangelmorato84@gmail.com>
2021-01-26 22:58:31 +0100

    merge

--- migrations/20210126161937-create-assignment.js
+++ migrations/20210126161937-create-assignment.js
@@ -1,43 +1,5 @@
 'use strict';
 module.exports = {
-<<<<<<< HEAD
-    up: async(queryInterface, Sequelize) => {
-        await queryInterface.createTable('Assignments', {
-            id: {
-                allowNull: false,
-                autoIncrement: true,
-                primaryKey: true,
-                type: Sequelize.INTEGER
-            },
-            fecha_ejecucion: {
-                type: Sequelize.DATE
-            },
-            id_turno: {
-                type: Sequelize.INTEGER
-            },
-            num_placa_crea_orden: {
-                type: Sequelize.STRING
-            },
-            firmada_todos: {
-                type: Sequelize.BOOLEAN
-            },
-            pdf_orden_firmada: {
-                type: Sequelize.STRING
-            },
-            createdAt: {
-                allowNull: false,
-                type: Sequelize.DATE
-            },
-            updatedAt: {
-                allowNull: false,
-                type: Sequelize.DATE
-            }
-        });
-    },
-    down: async(queryInterface, Sequelize) => {
-        await queryInterface.dropTable('Assignments');
-    }
-=======
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Assignments', {
       idOrden: {
@@ -74,5 +36,4 @@ module.exports = {
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Assignments');
   }
->>>>>>> 221ade1f7b7dc9df138f02f60463639e930b9b6d
 };
