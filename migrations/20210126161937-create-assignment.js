0d7ccb699871e3d71e4644dbcfcf35fa43a428a6
scarrasco85 <chechucarrasco@gmail.com>
2021-01-27 20:13:57 +0100

    changes

--- migrations/20210126161937-create-assignment.js
+++ migrations/20210126161937-create-assignment.js
@@ -1,27 +1,26 @@
 'use strict';
 module.exports = {
-<<<<<<< HEAD
     up: async(queryInterface, Sequelize) => {
         await queryInterface.createTable('Assignments', {
-            id: {
+            idOrden: {
                 allowNull: false,
                 autoIncrement: true,
                 primaryKey: true,
                 type: Sequelize.INTEGER
             },
-            fecha_ejecucion: {
+            fechaEjecucion: {
                 type: Sequelize.DATE
             },
-            id_turno: {
+            idTurno: {
                 type: Sequelize.INTEGER
             },
-            num_placa_crea_orden: {
+            numPlacaCreaOrden: {
                 type: Sequelize.STRING
             },
-            firmada_todos: {
+            firmadaTodos: {
                 type: Sequelize.BOOLEAN
             },
-            pdf_orden_firmada: {
+            pdfOrdenFirmada: {
                 type: Sequelize.STRING
             },
             createdAt: {
@@ -37,42 +36,4 @@ module.exports = {
     down: async(queryInterface, Sequelize) => {
         await queryInterface.dropTable('Assignments');
     }
-=======
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Assignments', {
-      idOrden: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      fechaEjecucion: {
-        type: Sequelize.DATE
-      },
-      idTurno: {
-        type: Sequelize.INTEGER
-      },
-      numPlacaCreaOrden: {
-        type: Sequelize.STRING
-      },
-      firmadaTodos: {
-        type: Sequelize.BOOLEAN
-      },
-      pdfOrdenFirmada: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Assignments');
-  }
->>>>>>> 221ade1f7b7dc9df138f02f60463639e930b9b6d
 };
--- models/assignment.js
+++ models/assignment.js
@@ -3,7 +3,6 @@ const {
     Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-<<<<<<< HEAD
     class Assignment extends Model {
         /**
          * Helper method for defining associations.
@@ -15,37 +14,14 @@ module.exports = (sequelize, DataTypes) => {
         }
     };
     Assignment.init({
-        fecha_ejecucion: DataTypes.DATE,
-        id_turno: DataTypes.INTEGER,
-        num_placa_crea_orden: DataTypes.STRING,
-        firmada_todos: DataTypes.BOOLEAN,
-        pdf_orden_firmada: DataTypes.STRING
+        fechaEjecucion: DataTypes.DATE,
+        idTurno: DataTypes.INTEGER,
+        numPlacaCreaOrden: DataTypes.STRING,
+        firmadaTodos: DataTypes.BOOLEAN,
+        pdfOrdenFirmada: DataTypes.STRING
     }, {
         sequelize,
         modelName: 'Assignment',
     });
     return Assignment;
-=======
-  class Assignment extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Assignment.init({
-    fechaEjecucion: DataTypes.DATE,
-    idTurno: DataTypes.INTEGER,
-    numPlacaCreaOrden: DataTypes.STRING,
-    firmadaTodos: DataTypes.BOOLEAN,
-    pdfOrdenFirmada: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Assignment',
-  });
-  return Assignment;
->>>>>>> 221ade1f7b7dc9df138f02f60463639e930b9b6d
 };
